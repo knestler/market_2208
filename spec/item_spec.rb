dca5634d3b7de0493e1c119f2878c7143f7033d5
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-20 11:15:23 -0400

    feat: item tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,19 @@
+require './lib/item'
+
+RSpec.describe Item do
+  before :each do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  it 'has attributes' do 
+    expect(@item1).to be_an_instance_of(Item)
+    expect(@item2).to be_an_instance_of(Item)
+
+    expect(@item2.name).to eq("Tomato")
+    expect(@item2.price).to eq(0.5)
+  end
+end
+
+
+
