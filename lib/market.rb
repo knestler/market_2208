9418457c682e5fd5d894c8ec820f3cf16b0b33de
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-20 13:59:15 -0400

    feat: overstocked and sorted item list'

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,25 @@ attr_reader :name, :vendors
     end
   end
 
+  def sorted_item_list
+    items = []
+    vendors.each do |vendor|
+      vendor.inventory.each do |item|
+        items << item[0].name if !items.include?(item[0].name)
+      end
+    end
+    items.sort
+  end
+
+  def overstocked_items
+    sum = Hash.new(0)
+    @vendors.each { |vendor| vendor.inventory.each { |item, qty| sum[item] += qty}}
+    overstocked = []
+      sum.each {|item, qty| overstocked << item if vendors_that_sell(item).count > 1 && qty > 50}
+    overstocked
+  end 
+    
+  def total_inventory
+    
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -43,4 +43,39 @@ RSpec.describe Market do
     expect(@vendor2.potential_revenue).to eq(345.00)
     expect(@vendor3.potential_revenue).to eq(48.75)
   end
+
+  it 'can inventory items' do 
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    # expect(@market.total_inventory).to eq(
+    #     ({
+    #       @item1=> {
+    #         quantity: 100,
+    #         vendors: [@vendor1, @vendor3]
+    #       },
+    #       @item2 => {
+    #         quantity: 7,
+    #         vendors: [@vendor1]
+    #       },
+    #       @item4 => {
+    #         quantity: 50,
+    #         vendors: [@vendor2]
+    #       },
+    #       @item3 => {
+    #         quantity: 35,
+    #         vendors: [@vendor2, @vendor3]
+    #       },
+    #     })
+
+      expect(@market.overstocked_items).to eq([@item1])
+      expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
+  end
 end
