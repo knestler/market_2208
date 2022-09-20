8f5ec07772eecd17e1925000331adc3aa8a9ae23
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-20 11:39:13 -0400

    fix: item tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Item do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
-
-  it 'has attributes' do 
+ 
+  it 'exists' do 
     expect(@item1).to be_an_instance_of(Item)
     expect(@item2).to be_an_instance_of(Item)
+  end
 
+  it 'has attributes' do 
     expect(@item2.name).to eq("Tomato")
     expect(@item2.price).to eq(0.5)
   end
